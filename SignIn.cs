160276b096c1c50553e43c2e7708e53938a1314c
manjusomu <manjusomu2001@gmail.com>
2024-02-15 12:07:23 +0530

    Add

---
+++ SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Authenticate (string username,string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(username)||string.IsNullOrEmpty(password))
+            {
+                msg = "User Name and Password required";
+            }
+            else
+            {
+                if((username=="admin")&& (password=="admin@123"))
+                {
+                    msg = "Authentication pass";
+                }
+                else
+                {
+                    msg = "Authentication fail";
+                }
+            }
+            return msg;
+        }
+    }
+}
